f5a28852c9c3e7d4f73b46064aa0ed4b432f82b1
refilwe-m <refilwe.dev@gmail.com>
2025-05-09 07:05:44 +0200

    Fixed deployment issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-
-// Import the generated route tree
 import { routeTree } from "./routeTree.gen";
-
 import "./styles.css";
-import reportWebVitals from "./reportWebVitals.ts";
 
-// Create a new router instance
 const router = createRouter({
 	routeTree,
 	context: {},
@@ -16,16 +11,15 @@ const router = createRouter({
 	scrollRestoration: true,
 	defaultStructuralSharing: true,
 	defaultPreloadStaleTime: 0,
+	basepath: "/star-wars-app",
 });
 
-// Register the router instance for type safety
 declare module "@tanstack/react-router" {
 	interface Register {
 		router: typeof router;
 	}
 }
 
-// Render the app
 const rootElement = document.getElementById("app");
 if (rootElement && !rootElement.innerHTML) {
 	const root = ReactDOM.createRoot(rootElement);
@@ -35,8 +29,3 @@ if (rootElement && !rootElement.innerHTML) {
 		</StrictMode>,
 	);
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
